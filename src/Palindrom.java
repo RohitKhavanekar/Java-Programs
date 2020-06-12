e3b8c11b657e3acc9be44e90070abde255573ade
Rohit Khavanekar <rohit.oyc9@gmail.com>
2020-06-12 11:34:52 +0530

    6-12-2020

---
+++ src/Palindrom.java
@@ -0,0 +1,20 @@
+
+
+public class Palindrom {
+
+	public static void main(String[] args) 
+	{
+		String a = "Rohit";
+		String b = "";
+		
+		for (int i =a.length()-1; i>=0; i--) 
+		{
+			b = b+ a.charAt(i);
+			
+		}
+		
+		System.out.println(b);
+
+	}
+
+}
